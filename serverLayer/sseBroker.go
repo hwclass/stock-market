f6d88149eac3df5191a11554e911fb68c0a73c59
hwclass <harderworking.class@gmail.com>
2016-06-14 15:24:01 +0300

    exported the struct as Stock and commented the import in sseBroker file

--- serverLayer/sseBroker.go
+++ serverLayer/sseBroker.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 	"github.com/garyburd/redigo/redis"
+	//Stock "./structs/Stock"
 )
 
 //	"encoding/json"
--- serverLayer/structs/Stock.go
+++ serverLayer/structs/Stock.go
@@ -4,4 +4,4 @@ type Stock struct {
   Id int64 `db:"id" json:"id"`
   Firm string `db:"firm" json:"firm"`
   Data string `db:"data" json:"data"`
-} 
+}
